4f716d9bbc8aa1469062b4720a6d19d4acbb2cf0
Rossel Thompson <rossel96t@gmail.com>
2022-07-02 02:23:10 -0600

    create navbar component

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -3,11 +3,13 @@ import { ILayout } from 'types/Interfaces/Layout/ILayout';
 import { Content } from './Content/Content';
 import { Footer } from './Footer/Footer';
 import { Header } from './Header/Header';
+import { Navbar } from './Navbar/Navbar';
 
 export const Layout: React.FC<ILayout> = (props): JSX.Element => {
   const { children } = props;
   return (
     <>
+      <Navbar />
       <Header />
       <Content>{children}</Content>
       <Footer />
--- components/Layout/Navbar/Navbar.tsx
+++ components/Layout/Navbar/Navbar.tsx
@@ -1,5 +1,51 @@
-const Navbar: React.FC = (): JSX.Element => {
-  return <div>Navbar</div>;
-};
+import Image from 'next/image';
 
-export default Navbar;
+export const Navbar: React.FC = (): JSX.Element => {
+  const onSubmitSearchInput = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+  };
+  return (
+    <nav className="nav" role="navigation">
+      <div className="max-centered">
+        <h1 className="nav__logo">Rule of thumb.</h1>
+        <button className="nav__hamburger icon-button">
+          <svg width="25" height="20" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M0 0h25v4H0V0zm0 8h25v4H0V8zm0 8h25v4H0v-4z"
+              fill="#FFF"
+              fillRule="nonzero"
+            />
+          </svg>
+        </button>
+        <ul className="nav__links">
+          <li>
+            <a href="#">Past Trials</a>
+          </li>
+          <li>
+            <a href="#">How It Works</a>
+          </li>
+          <li>
+            <a href="#">Login / Sign Up</a>
+          </li>
+          <li>
+            <form onSubmit={onSubmitSearchInput}>
+              <input
+                className="nav__search-input"
+                aria-label="search"
+                type="text"
+              />
+              <button className="nav__search icon-button" type="submit">
+                <Image
+                  src="/images/search.svg"
+                  alt="search"
+                  width="100%"
+                  height="100%"
+                />
+              </button>
+            </form>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
